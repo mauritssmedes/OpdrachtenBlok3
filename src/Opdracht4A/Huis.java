7980934f28f5e52ac951bf83ea87586e3b4243ce
Maurits Smedes <mauritssmedes@hotmail.com>
2021-02-12 23:05:46 +0100

    3B toegevoegd

--- src/Opdracht4A/Huis.java
+++ src/Opdracht4A/Huis.java
@@ -2,5 +2,21 @@ package Opdracht4A;
 
 public class Huis {
     private String adres;
+    private int bouwjaar;
+    Persoon huisbaas = null;
 
+    public Huis(String adres, int bouwjaar){
+        this.adres = adres;
+        this.bouwjaar = bouwjaar;
+    }
+    public void setHuisbaas(Persoon hb){
+        this.huisbaas = hb;
+    }
+    public Persoon getHuisbaas(){
+        return huisbaas;
+    }
+    public String toString(){
+        return "Huis " + adres + " is gebouwd in " + bouwjaar + "\n" +
+                "en heeft nu huisbaas " + getHuisbaas().toString();
+    }
 }
--- src/Opdracht4A/Persoon.java
+++ src/Opdracht4A/Persoon.java
@@ -4,4 +4,11 @@ public class Persoon {
     private String naam;
     private int leeftijd;
 
+    public Persoon(String nm, int lft){
+        this.naam = nm;
+        this.leeftijd = lft;
+    }
+    public String toString(){
+        return naam + "; leeftijd " + leeftijd + " jaar";
+    }
 }
